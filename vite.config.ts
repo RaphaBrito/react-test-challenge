07ad39c6c7d11261bd916b73fd40b552930f59d5
Marcello Cordeiro <23259818+marcellocordeiro@users.noreply.github.com>
2023-11-30 18:48:57 -0300

    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+  },
   plugins: [react()],
-})
+});
