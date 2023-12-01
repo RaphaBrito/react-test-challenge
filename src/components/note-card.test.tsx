1f412f2d965b7556ef244a447174e0392e93596c
alexandrazarzar <avz@cin.ufpe.br>
2023-12-01 16:28:08 -0300

    Note Card component tests

---
+++ src/components/note-card.test.tsx
@@ -0,0 +1,80 @@
+import { expect, it, vi } from "vitest";
+
+import {
+    NoteCard,
+    type NoteCardProps,
+  } from "./note-card";
+  import { render, screen, userEvent } from "../test/utils";
+  import { type Note } from "../types/note";
+
+const mockNote = {
+  id: 1,
+  title: "Test Note",
+  description: "Test Description",
+} satisfies Note;
+
+describe("NoteCard component", () => {
+    it("should render correctly with an existing note", () => {
+        const note = {
+            id: 0,
+            title: "note title",
+            description: "note description"
+          } satisfies Note;
+
+        const props = {
+        note,
+          onEdit: () => undefined,
+          onDelete: () => undefined,
+        } satisfies NoteCardProps;
+      
+        const result = render(<NoteCard {...props} />); 
+
+        expect(result).toMatchSnapshot();
+      });
+
+    it("when 'Editar' button is clicked, it should disappear", async() => {
+        const onEdit = vi.fn()
+
+        const note = {
+            id: 0,
+            title: "note title",
+            description: "note description"
+        } satisfies Note;
+
+        const props = {
+            note,
+            onEdit,
+            onDelete: () => undefined,
+        } satisfies NoteCardProps;
+
+        const result = render(<NoteCard {...props} />);
+
+        await userEvent.click(screen.getByText("Editar"));
+
+        expect(screen.queryByText("Editar")).not.toBeInTheDocument();
+        expect(screen.queryByText("Salvar")).toBeInTheDocument();
+        expect(screen.queryByText("Cancelar")).toBeInTheDocument();
+    });
+
+  it("should call onDelete when 'Remover' button is clicked", async() => {
+    const onDelete = vi.fn()
+
+    const note = {
+        id: 0,
+        title: "note title",
+        description: "note description"
+    } satisfies Note;
+
+    const props = {
+        note,
+        onEdit: () => undefined,
+        onDelete
+    } satisfies NoteCardProps;
+
+    const result = render(<NoteCard {...props} />);
+
+    await userEvent.click(screen.getByText("Remover"));
+
+    expect(onDelete).toHaveBeenCalledWith(note.id);
+  });
+});
